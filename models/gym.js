bade2c4bf89328b1d7e2c3e3a6f60a363f5e7beb
Goodbyez <miradadesigual@gmail.com>
2021-06-03 13:25:55 +0200

    Create gym model

---
+++ models/gym.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const gymSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    adress: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    cp: {
+        type: Number,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true,
+    },
+    lessons: [{
+        type: mongoose.Schema.Types.ObjectId, ref: "Lesson"
+    }],
+    country: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+
+
+const toJSONConfig = {
+    transform: (doc,ret,opt) => {
+           delete ret['password']
+           return ret
+    }
+}
+coachSchema.set('toJSON', toJSONConfig);
+
+
+const Coach = mongoose.model('Coach', coachSchema);
+module.exports = Coach;
